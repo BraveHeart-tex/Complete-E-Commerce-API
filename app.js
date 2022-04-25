e6b11636fb6324a09c1607879bfb302e566413b9
Bora Karaca <bora.karaca@ogr.iu.edu.tr>
2022-04-25 17:02:02 +0300

    fixed bugs

---
+++ app.js
@@ -0,0 +1,75 @@
+// dotenv
+require('dotenv').config();
+require('express-async-errors');
+
+// express
+const express = require('express');
+const app = express();
+
+// rest of the packages
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
+
+// DB connection
+const connectDB = require('./db/connect');
+
+// routers
+const authRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
+const reviewRouter = require('./routes/reviewRoutes');
+const orderRouter = require('./routes/orderRoutes');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowsMs: 15 * 60 * 1000,
+    max: 60,
+  })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
+app.use(morgan('tiny'));
+app.use(express.json()); // get access to req.body JSON data
+app.use(cookieParser(process.env.JWT_SECRET)); // get access to req.cookies
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
+app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/orders', orderRouter);
+
+app.use(express.static('./public'));
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    // Connect to DB
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`Server listening on ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
